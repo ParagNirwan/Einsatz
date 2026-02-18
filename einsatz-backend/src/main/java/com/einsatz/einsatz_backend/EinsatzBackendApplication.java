5b8f29935c8a1e7ead07c236842ef6f67b4e25f2
Parag Nirwan <pnirwan9@gmail.com>
2026-02-18 22:12:21 +0530

    First Commit

---
+++ einsatz-backend/src/main/java/com/einsatz/einsatz_backend/EinsatzBackendApplication.java
@@ -0,0 +1,13 @@
+package com.einsatz.einsatz_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EinsatzBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EinsatzBackendApplication.class, args);
+	}
+
+}
---
+++ einsatz-backend/src/test/java/com/einsatz/einsatz_backend/EinsatzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.einsatz.einsatz_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EinsatzBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
