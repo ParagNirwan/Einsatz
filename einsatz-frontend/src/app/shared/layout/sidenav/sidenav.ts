666ff91a9c2c315809759724b1c49a8a95862f56
Parag Nirwan <pnirwan9@gmail.com>
2026-02-23 16:28:11 +0530

    Logout Feature

--- einsatz-frontend/src/app/shared/layout/sidenav/sidenav.ts
+++ einsatz-frontend/src/app/shared/layout/sidenav/sidenav.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 @Component({
   selector: 'app-sidenav',
   standalone: true,
@@ -23,10 +24,26 @@ import { RouterModule } from '@angular/router';
 })
 export class Sidenav {
 
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
   menuItems = [
     { label: 'Dashboard', route: '/dashboard' },
     { label: 'Users', route: '/users' },
-    { label: 'Settings', route: '/settings' }
+    { label: 'Settings', route: '/settings' },
+    {label: 'Logout', action: 'logout'}
   ];
+
+  handleClick(item:any){
+    if(item.action === 'logout'){
+      this.logout();
+    }
+  }
+
+  logout(){
+  
+    this.authService.logout();
+    this.router.navigate(['/auth/login'])
+  }
 
 }
